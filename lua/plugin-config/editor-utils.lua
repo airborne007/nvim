d867862d48aef4933957e9a1124bb17257816834
airborne007 <isrfhltc@outlook.com>
2026-03-28 19:59:38 +0800

    fix(surround): update nvim-surround config for v4 API
    nvim-surround v4 removed keymaps from setup(), now uses vim.g
    options and vim.keymap.set() instead.

--- lua/plugin-config/editor-utils.lua
+++ lua/plugin-config/editor-utils.lua
@@ -11,8 +11,10 @@ return {
     config = function()
       require("nvim-surround").setup()
 
-      -- Remap visual surround to gS to save S for Flash
-      vim.keymap.set("v", "gS", "<Plug>(nvim-surround-visual)", { desc = "Surround visual selection" })
+      -- Disable default visual S mapping (conflicts with Flash)
+      vim.g.nvim_surround_no_visual_mappings = true
+      -- Map visual surround to gS
+      vim.keymap.set("x", "gS", "<Plug>(nvim-surround-visual)", { desc = "Surround visual selection" })
     end
   },
 }
